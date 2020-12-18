d8fd131643e27f351e99c619a2f2d3db0d84aeba
OrangeX4 <318483724@qq.com>
2020-12-18 21:47:36 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const command = require('child_process');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    command.exec(`start http://127.0.0.1:${port}`);
+})
+
