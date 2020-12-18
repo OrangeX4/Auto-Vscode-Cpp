b2457b791a0639773a0273bff41bc1cfc2071212
OrangeX4 <318483724@qq.com>
2020-12-19 00:46:51 +0800

    Add some function

--- lanzouLinkResolutor.js
+++ lanzouLinkResolutor.js
@@ -1,14 +1,14 @@
 const request = require('request')
+// var FormData = require('form-data')
 
 function Resolve(LanzouLink, callback) {
     request(LanzouLink, null, (err, res, body) => {
         if (err) return
-        SolveDownloadPageUri(body, (downloadPageUri) => {
-            let downloadUrl = SolveDownloadUrl(downloadPageUri)
+        downloadPageUri = SolveDownloadPageUri(body)
+        SolveDownloadUrl(downloadPageUri, (downloadUrl) => {
             callback(downloadUrl)
         })
     })
-
 }
 
 function SolveDownloadUrl(downloadPageUrl, callback) {
@@ -17,80 +17,80 @@ function SolveDownloadUrl(downloadPageUrl, callback) {
         let sign = ""
         let signValue = ""
         let data = ""
-    
+
         // 获取Content中JS部分
-        let jsRegex = new RegExp('<script type="text/javascript">\\n([\\S\\s]*?)</script>', 'g')
+        let jsRegex = new RegExp('<script type="text/javascript">\\n([\\S\\s]*?)</script>')
         let jsMatch = content.match(jsRegex)
-    
-        if (!jsMatch) {
-            jsContent = jsMatch[0]
-    
+
+        if (jsMatch) {
+            let jsContent = jsMatch[1]
+
             // 注释替换表达式
-            let descriptionRegex = new RegExp("//.*?[\\n]", 'g');
-            let jsContent = jsContent.replace(descriptionRegex, "\n")
-    
+            let descriptionRegex = new RegExp("//.*?[\\n]");
+            jsContent = jsContent.replace(descriptionRegex, "\n")
+
             // 获取data值
-            let dataRegex = new RegExp("data : (.*?,'sign':(.*?),.*?})", 'g');
+            let dataRegex = new RegExp("data : (.*?,'sign':(.*?),.*?})");
             let dataMatch = jsContent.match(dataRegex);
-    
-            if (!dataMatch) {
-                let data = dataMatch[0];
-                sign = dataMatch[1];
-    
+
+            if (dataMatch) {
+                data = dataMatch[1];
+                sign = dataMatch[2];
+
                 //根据Sign获取Sign值
-                let signRegex = new RegExp("var " + sign + " *?= *?'(.*?)';", 'g');
+                let signRegex = new RegExp("var ajaxdata *?= *?'(.*?)';");
                 let signMatch = jsContent.match(signRegex);
-    
-                if (!signMatch) {
+
+                if (signMatch) {
                     signValue = signMatch[1];
                     data = data.replace("'sign':" + sign, "'sign':'" + signValue + "'");
                 }
             }
         }
-    
+
         //转化data为键值对
         // data = Json2FormData(data)
         // data = Encoding.UTF8.GetString(Encoding.Default.GetBytes(data))
         // let phpContent = PostAjax(data, downloadPageUrl)
-    
-        let options = {
-            uri: downloadPageUrl,
-            method: 'POST',
-            json: data
-        }
-    
-        request(options, function (error, response, body) {
+
+        let r = request.post(downloadPageUrl, null, (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 let phpContent = body
                 let finalUrl = "";
-                let domRegex = new RegExp("\"dom\":\"(.*)\",\"url\"", 'g')
-                let urlRegex = new RegExp("\"url\":\"(.*)\",\"inf\"", 'g')
-            
+                let domRegex = new RegExp("\"dom\":\"(.*)\",\"url\"")
+                let urlRegex = new RegExp("\"url\":\"(.*)\",\"inf\"")
+
                 let domMatch = phpContent.match(domRegex)
                 let urlMatch = phpContent.match(urlRegex)
-            
-                if (domMatch.Success) {
-                    finalUrl = domMatch[0].replace("\\", "")
+
+                if (domMatch) {
+                    finalUrl = domMatch[1].replace("\\", "")
                 }
-            
+
                 finalUrl += "/file/"
-            
-                if (urlMatch.Success) {
-                    finalUrl += urlMatch[0].replace("\\", "")
+                if (urlMatch) {
+                    finalUrl += urlMatch[1].replace("\\", "")
                 }
-            
+
                 callback(finalUrl)
             }
         })
+
+        let form = r.form()
+        data = JSON.parse(data.replace(/'/g, '"'))
+        Object.keys(data).forEach((key) => {
+            form.append(key, data[key])
+        })
+
     })
 }
 
 function SolveDownloadPageUri(content) {
-    let regex = new RegExp("<iframe class=\"ifr2\" name=\"[0-9]+\" src=\"(.+)\" frameborder=\"0\" scrolling=\"no\"></iframe>\n", 'g')
+    let regex = new RegExp("<iframe class=\"ifr2\" name=\"[0-9]+\" src=\"((.+))\" frameborder=\"0\" scrolling=\"no\"></iframe>\n")
     let match = content.match(regex)
     let relativeUrl = ''
-    if (!match) {
-        relativeUrl = match[0]
+    if (match) {
+        relativeUrl = match[1]
     }
 
     if (relativeUrl !== '') {
@@ -102,5 +102,5 @@ function SolveDownloadPageUri(content) {
 
 
 Resolve("https://wwe.lanzous.com/i9T15jhtm0h", (url) => {
-    print(url)
+    console.log(url)
 })
