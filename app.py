e1a36fd2c3b013e4f18539b9ca52cb59432e656d
OrangeX4 <318483724@qq.com>
2020-12-19 14:32:42 +0800

    Change to static

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from api import get_zhilian, folderSelector
 from webbrowser import open as webopen
 import requests
@@ -6,9 +6,10 @@ from os import environ
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "hello"
+    return redirect('/static/index.html')
 
 
 @app.route('/geturl')
@@ -17,16 +18,18 @@ def test():
         url = request.args.get("url")
     except:
         url = ''
-    r = requests.get(get_zhilian(url)) 
+    r = requests.get(get_zhilian(url))
     # http://127.0.0.1:5000/geturl?url=https://www.lanzous.com/i6aa3hg
     with open("demo3.zip", "wb") as code:
         code.write(r.content)
+
 
 @app.route('/select')
 def select():
     return folderSelector()
 
+
 if __name__ == '__main__':
     # print(environ['path'])
-    webopen('http://127.0.0.1:5000')
-    app.run()
+    webopen('http://127.0.0.1:5200')
+    app.run(host='127.0.0.1', port=5200)
