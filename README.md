186326d2f8b9072447c9a1fe98feb81a8c3d2b88
OrangeX4 <318483724@qq.com>
2020-12-19 16:54:41 +0800

    Add most of functions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Auto-Vscode-Cpp
 
 自动安装Vscode和Cpp必要的环境.
+
+## 运行
+
+```bash
+python app.py
+```
--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import sys
 from flask import Flask, request, redirect
 from api import get_zhilian, folderSelector
 from webbrowser import open as webopen
 import requests
-from os import environ
+from os import environ, system, path, remove
+import py7zr
 
 app = Flask(__name__)
+
+# 跨域支持
+
+
+def after_request(resp):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+app.after_request(after_request)
 
 
 @app.route('/')
@@ -12,24 +24,70 @@ def index():
     return redirect('/static/index.html')
 
 
-@app.route('/geturl')
-def test():
-    try:
-        url = request.args.get("url")
-    except:
-        url = ''
-    r = requests.get(get_zhilian(url))
-    # http://127.0.0.1:5000/geturl?url=https://www.lanzous.com/i6aa3hg
-    with open("demo3.zip", "wb") as code:
-        code.write(r.content)
-
-
 @app.route('/select')
 def select():
     return folderSelector()
 
 
+@app.route('/isVscode')
+def isVscode():
+    return ('vs code' in environ['path'].lower() or 'vscode' in environ['path'].lower()) and 'true' or 'false'
+
+
+@app.route('/isMinGW')
+def isMinGW():
+    return 'mingw' in environ['path'].lower() and 'true' or 'false'
+
+
+@app.route('/vscode')
+def vscode():
+    r = requests.get(get_zhilian("https://wwe.lanzous.com/imCQzjig9yb"))
+    with open("install.exe", "wb") as code:
+        code.write(r.content)
+    system('install.exe')
+    if path.exists("install.exe"):
+        remove("install.exe")
+    return 'true'
+
+
+@app.route('/mingw')
+def mingw():
+    try:
+        pathStr = request.args.get('path')
+    except:
+        pathStr = 'D:\\Program\\MinGW'
+    r = requests.get(get_zhilian('https://wwe.lanzous.com/iqjyBjih6qb'))
+    with open("MinGW.7z", "wb") as code:
+        code.write(r.content)
+    archive = py7zr.SevenZipFile('MinGW.7z', mode='r')
+    archive.extractall(path=pathStr)
+    archive.close()
+    aftPath = environ['path'] + path.join(pathStr, '/MinGw/bin') + ';'
+    command = r"setx WORK1 %s /m" % aftPath
+    environ['path'] = aftPath
+    system(command)
+    if path.exists("MinGW.7z"):
+        remove("MinGW.7z")
+    return 'true'
+
+
+@app.route('/codespace')
+def codespace():
+    try:
+        pathStr = request.args.get("path")
+    except:
+        pathStr = 'D:\\CodeSpace'
+    r = requests.get(get_zhilian('https://wwe.lanzous.com/ixnHGjihzwb'))
+    with open("CodeSpace.7z", "wb") as code:
+        code.write(r.content)
+    archive = py7zr.SevenZipFile('CodeSpace.7z', mode='r')
+    archive.extractall(path=pathStr)
+    archive.close()
+    if path.exists("CodeSpace.7z"):
+        remove("CodeSpace.7z")
+    return 'true'
+
+
 if __name__ == '__main__':
-    # print(environ['path'])
     webopen('http://127.0.0.1:5200')
     app.run(host='127.0.0.1', port=5200)
